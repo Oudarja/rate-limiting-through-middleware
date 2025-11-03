63a97eb6b90f47e1b85d2ffeeaeb4423343ebde5
oudarja <oudarja360@gmail.com>
2025-11-03 15:41:09 +0600

    classbased middleware for rate limiting done

--- ClassBasedModlleware.py
+++ ClassBasedModlleware.py
@@ -7,3 +7,67 @@ app.add_middleware()
 '''
 
 
+import time
+from fastapi import FastAPI, Request, HTTPException, status
+# BaseHTTPMiddleware →a Starlette class inherited from to create class-based middleware 
+# in FastAPI.
+from starlette.middleware.base import BaseHTTPMiddleware
+
+MAX_CALLS = 5       # Max requests allowed
+TIME_FRAME = 60     # Time window in seconds
+
+
+class RateLimiterMiddleware(BaseHTTPMiddleware):
+    # __init__ → called when the middleware is added to the app.
+    def __init__(self,app):
+        # initializes the base middleware with the FastAPI app instance.
+        super().__init__(app)
+        # {client_ip: [timestamps]}
+        self.rate_limit_store = {}
+
+    async def dispatch(self,request:Request,call_next):
+
+        client_ip=request.client.host
+
+        now=time.time()
+
+        # Get list of previous calls for this IP, or initialize
+        calls = self.rate_limit_store.get(client_ip, [])
+
+        # Keep only timestamps within the last TIME_FRAME seconds
+        recent_calls = [ts for ts in calls if ts >= now - TIME_FRAME]
+
+        # Update store
+        self.rate_limit_store[client_ip] = recent_calls
+
+        # Check if client exceeded limit
+        if len(recent_calls) >= MAX_CALLS:
+            raise HTTPException(
+                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+                detail="Rate limit exceeded. Try again later.",
+            )
+
+        # Record this call
+        self.rate_limit_store[client_ip].append(now)
+
+        if(len(self.rate_limit_store[client_ip])>MAX_CALLS):
+            del self.rate_limit_store[client_ip][:-MAX_CALLS]
+
+        # Proceed to next middleware or route handler
+        response = await call_next(request)
+        return response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main_.py
+++ main_.py
@@ -1,26 +1,23 @@
 from fastapi import FastAPI , Request
 import random
 import string
-from FunctionBasedMiddleware import register_rate_limit_middleware
+# from FunctionBasedMiddleware import register_rate_limit_middleware
 from ClassBasedModlleware import RateLimiterMiddleware
 
 app=FastAPI()
 
 
-register_rate_limit_middleware(app)
+# register_rate_limit_middleware(app)
 
-
+app.add_middleware(RateLimiterMiddleware)
 
 @app.get("/")
 async def read_root():
-    return {"message": "Welcome! You’re within the rate limit. I am from main"}
+    return {"message": "Welcome! You’re within the rate limit. I am from main_"}
 
 
 @app.get("/status")
 async def check_status():
     return {"status": "Server is running"}
-
-
-
 
 
